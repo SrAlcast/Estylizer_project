00dc7dd89ecfab1fd8642beb4c3b50d88588c383
Alex <alexcastrovarela@gmail.com>
2025-02-04 16:16:01 +0100

    Update Mobile.py

--- Streamlit/pages/Mobile.py
+++ Streamlit/pages/Mobile.py
@@ -164,16 +164,35 @@ st.markdown(
     unsafe_allow_html=True,
 )
 
-# Mostrar logo centrado
-col1, col2, col3 = st.columns([1, 2, 1])  # Centrar imagen en la columna central
-with col2:
-    st.image("./src/Logo Estylizer 2.png", width=200)
+
+
+st.markdown(
+    """
+    <style>
+    .centered-image img {
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+st.markdown('<div class="centered-image">', unsafe_allow_html=True)
+st.image("./src/Logo Estylizer 2.png", width=200)
+st.markdown('</div>', unsafe_allow_html=True)
+
+# # Mostrar logo centrado
+# col1, col2, col3 = st.columns([1, 2, 1])  # Centrar imagen en la columna central
+# with col2:
+#     st.image("./src/Logo Estylizer 2.png", width=200)
 
 # Página 1: Selección de modelo_tags
 if st.session_state.page == 1:
     if st.session_state.modelo_tags_index < 8:
         modelo_tags = modelos_tageados.iloc[st.session_state.random_indices[st.session_state.modelo_tags_index]]
-        st.image(modelo_tags['image1_url'], width=270) 
+        st.image(modelo_tags['image1_url'], width=260) 
         st.markdown("<div class='centered-container'>", unsafe_allow_html=True)
         dislike_pressed = st.button("❌ No me gusta", key=f"dislike_{st.session_state.modelo_tags_index}")
         like_pressed = st.button("✅ Me gusta", key=f"like_{st.session_state.modelo_tags_index}")
