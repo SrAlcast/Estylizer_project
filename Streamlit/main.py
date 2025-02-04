a95a7cb82e3e658b0a10b03c4f534c8f2ef6235e
Alex <alexcastrovarela@gmail.com>
2025-02-04 15:43:26 +0100

    Update main.py

--- Streamlit/main.py
+++ Streamlit/main.py
@@ -10,7 +10,7 @@ sys.path.append("./src/")
 import support_mongo as sm
 
 # Configuración de la página
-st.set_page_config(page_title="Estylizer - Recomendaciones", layout="centered")
+st.set_page_config(page_title="Estylizer - Recomendaciones")
 
 def recomendador_superior(productos, tags_aceptados_general, tags_aceptados_superior, tags_rechazados_general, tags_rechazados_superior, tipos_superior, colores_superior, presupuesto_superior_min, presupuesto_superior_max):
     
