ad5a561500a3a648a4c8eb060f316752b8e0c488
Alex <alexcastrovarela@gmail.com>
2025-02-04 17:01:03 +0100

    Update Mobile.py

--- Streamlit/pages/Mobile.py
+++ Streamlit/pages/Mobile.py
@@ -505,7 +505,7 @@ elif st.session_state.page == 4:
 
 # Página 5: Mostrar recomendaciones
 elif st.session_state.page == 5:
-    st.subheader( divider="blue")
+    st.markdown("---")
 
     presupuesto_superior = st.session_state.presupuesto_superior
     presupuesto_inferior = st.session_state.presupuesto_inferior
