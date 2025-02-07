61f42efd28f7ea4891c17504595041a8da39fc3f
Alex <alexcastrovarela@gmail.com>
2025-02-07 21:17:46 +0100

    Update main.py

--- Streamlit/main.py
+++ Streamlit/main.py
@@ -8,7 +8,6 @@ st.write("Selecciona la versión que deseas utilizar:")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.page_link("pages/mobile.py")
-
-with col2:
     st.page_link("pages/desktop.py")
+with col2:
+    st.page_link("pages/mobile.py")
