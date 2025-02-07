fe0646e5b562dc8817a88c4a715e6a46a5b4de03
Alex <alexcastrovarela@gmail.com>
2025-02-07 20:42:17 +0100

    Update Mobile.py

--- Streamlit/pages/Mobile.py
+++ Streamlit/pages/Mobile.py
@@ -8,7 +8,7 @@ import sys
 sys.path.append("./src/")
 import support_mongo as sm
 
-st.set_page_config(page_title="Estylizer - Recomendador de ropa", layout="centered", page_icon="👕")
+st.set_page_config(page_title="Estylizer - Recomendador de ropa", layout="centered", page_icon="https://raw.githubusercontent.com/SrAlcast/Estylizer_project/refs/heads/main/src/icon.png")
 
 def recomendador_superior(productos, tags_aceptados_general, tags_aceptados_superior, tags_rechazados_general, tags_rechazados_superior, tipos_superior, colores_superior, presupuesto_superior_min, presupuesto_superior_max):
     
