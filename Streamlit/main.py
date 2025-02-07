6d26ff81aea73191ee3059215722d08401212e18
Alex <alexcastrovarela@gmail.com>
2025-02-07 21:21:36 +0100

    Update main.py

--- Streamlit/main.py
+++ Streamlit/main.py
@@ -2,6 +2,74 @@ import streamlit as st
 
 st.set_page_config(page_title="Selector de versión", layout="centered")
 
+st.markdown(
+    """
+    <style>
+    .block-container {
+        padding-top: 2rem !important;
+        margin: auto;
+    }
+    .stButton > button {
+        width: 100%;
+        border-radius: 10px;
+        font-size: 16px;
+    }
+    .title-text {
+        text-align: center;
+        font-size: 28px;
+        font-weight: bold;
+        color: #ff5733;
+    }
+    .centered-container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        flex-direction: column;
+    }
+    .image-container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+    }
+    .image-container img {
+        max-width: 100%;
+        height: auto;
+        border-radius: 10px;
+    }
+    .centered-title {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+        font-size: 22px;
+        font-weight: bold;
+        color: white;
+    }
+    .divider {
+        width: 50%;
+        margin: auto;
+        border: 1px solid red; /* Línea muy fina */
+    }
+    .centered {
+        text-align: center;
+    }
+    .coste-total {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+        font-size: 18px;
+        font-weight: bold;
+        color: white;
+        margin-top: 5px;
+        margin-bottom: 5px;
+        }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
 # Imagen logo superior
 st.markdown(f"""
 <div class='image-container' style="margin-top: 15px; margin-bottom: 20px;">
@@ -13,6 +81,6 @@ col1, col2 = st.columns(2)
 
 with col1:
     st.page_link("pages/desktop.py")
-    
+
 with col2:
     st.page_link("pages/Mobile.py")
